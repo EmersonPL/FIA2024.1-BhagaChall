4427a0356db7cbfbe737dd3e2c693c74d08f9a3d
Emerson Lopes <eplopes@inf.ufpel.edu.br>
2024-07-27 22:29:32 -0300

    Add victory conditions

--- src/game/game.py
+++ src/game/game.py
@@ -1,16 +1,7 @@
-from typing import List, Literal
+from typing import List, Literal, Tuple
 
-import numpy as np
-import numpy.typing as npt
-
-from src.constants import (
-    BOARD_COLS,
-    BOARD_LINES,
-    GOAT_PLAYER,
-    TIGER_PLAYER,
-    TOTAL_NUMBER_OF_GOATS,
-)
-from src.game.game_types import BoardSquare, Capture, Movement, Play
+from src.constants import GOAT_PLAYER, TIGER_PLAYER
+from src.game.game_types import Play
 from src.game.game_state import GameState
 from src.game.board import Board
 
@@ -23,3 +14,13 @@ class Game:
     def available_moves(self) -> List[Play]:
         """Return a list of the available moves."""
         return self.board.available_moves(self.game_state)
+
+    def is_game_over(self) -> Tuple[bool, Literal[-1, 1] | None]:
+        """Return a tuple indicating if the game is over and it's winner."""
+        if self.game_state.captured_goats >= 5:
+            return True, TIGER_PLAYER
+
+        if self.game_state.player == TIGER_PLAYER and len(self.board.available_moves(self.game_state)) == 0:
+            return True, GOAT_PLAYER
+
+        return False, None
